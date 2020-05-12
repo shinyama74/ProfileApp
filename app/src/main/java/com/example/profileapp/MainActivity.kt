0d880b4dd85ec1003463f81f7e035ca0851cf2b4
shinyama <funmon0704@gmail.com>
2020-05-12 18:22:56 +0900

    profileApp completed!

---
+++ app/src/main/java/com/example/profileapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.profileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        profileImage.isVisible=false
+        profileCommentText.isVisible=false
+
+        button1.setOnClickListener {
+            profileImage.isVisible = true
+            profileCommentText.isVisible=true
+
+        }
+    }
+}
